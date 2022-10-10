01f310352b51a44d5fb1c67c7cd3f51ebaa88c5f
Olvenn <olga-vencel@yandex.ru>
2022-10-10 14:49:23 +0300

    Task 5. Add function for update posts.

---
+++ src/update-posts.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+import getParsedData from './parser.js';
+
+const updatePosts = (watched) => {
+  const { posts } = watched;
+
+  const feedsLinks = watched.linkUrl;
+  console.log('feedsLinks', feedsLinks);
+
+  feedsLinks.map((url) => axios({
+    url: `https://allorigins.hexlet.app/get?disableCache=false&url=${encodeURIComponent(url.trim())}`,
+  })
+    .then((response) => {
+      const data = getParsedData(response.data.contents);
+      const { postsData } = data;
+      const postsLinks = watched.posts.map((post) => post.link);
+      const newPosts = postsData.filter((post) => !postsLinks.includes(post.link));
+      posts.unshift(...newPosts);
+    }));
+
+  const timerId = setTimeout(() => {
+    updatePosts(watched);
+  }, '5000');
+  return timerId;
+};
+
+export default updatePosts;
